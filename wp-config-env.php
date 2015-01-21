08d559e8405ab674905d8c5bb24bbb1d79230d07
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2015-01-21 23:02:24 +0900

    Heroku database settings

--- wp-config-env.php
+++ wp-config-env.php
@@ -8,3 +8,24 @@ if ( $wp_config['wp_env'] === 'production' ) {
 	$wp_config['wp_debug'] = true;
 	$wp_config['disallow_file_mods'] = false;
 }
+
+/* Heroku settings */
+
+/* Database */
+if ( $wp_config['db_host'] === false ) {
+	$wp_config['heroku_db_list'] = [
+		'CLEARDB_DATABASE_URL',
+		'DATABASE_URL',
+	];
+	foreach ( $wp_config['heroku_db_list'] as $heroku_db ) {
+		if ( getenv($heroku_db) ) {
+			$wp_config['db_url'] = parse_url(getenv($heroku_db));
+			$wp_config['db_name'] = trim($wp_config['db_url']['path'], '/');
+			$wp_config['db_user'] = $wp_config['db_url']['user'];
+			$wp_config['db_password'] = $wp_config['db_url']['pass'];
+			$wp_config['db_host'] = $wp_config['db_url']['host'];
+			break;
+		}
+	}
+	unset($heroku_db);
+}
