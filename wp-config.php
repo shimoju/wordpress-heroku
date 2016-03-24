13c026fd41ed515272dc4ed681198709fd6c8e59
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-03-24 12:53:39 +0900

    Set database charset utf8mb4

--- wp-config.php
+++ wp-config.php
@@ -66,7 +66,7 @@ define('DB_PASSWORD', $wp_config['db_password']);
 define('DB_HOST', $wp_config['db_host']);
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
